25b0001e058ccff982a085f008e946df229c0080
walter lacoste <wlacoste@outlook.com>
2023-01-14 16:39:27 -0300

    modulo 1

--- Integrador1.sql
+++ Integrador1.sql
@@ -1,10 +1,9 @@
+-- Etapa 1.1
 drop database if exists LABORATORIO;
 
 CREATE SCHEMA LABORATORIO ;
 
 USE laboratorio;
-
-CREATE TABLE facturas;
 
 CREATE TABLE facturas (
   `letra` CHAR NOT NULL,
@@ -31,3 +30,21 @@ CREATE TABLE clientes (
   `direccion` VARCHAR(50) NULL,
   `comentarios` VARCHAR(50) NULL,
   PRIMARY KEY (`ClienteID`));
+  
+  
+-- Etapa 1.2
+ALTER TABLE facturas RENAME COLUMN ClienteID TO IDCliente;
+ALTER TABLE FACTURAS RENAME COLUMN ArticuloID TO IDArticulo;
+ALTER TABLE FACTURAS MODIFY monto int unsigned;
+  
+ALTER TABLE articulos RENAME COLUMN ArticuloID TO IDArticulo;
+ALTER TABLE articulos MODIFY nombre varchar(75) ;
+ALTER TABLE articulos MODIFY precio double unsigned not null;
+ALTER TABLE articulos MODIFY stock int unsigned not null;
+
+ALTER TABLE clientes RENAME COLUMN ClienteID to IDCliente;
+ALTER TABLE clientes MODIFY nombre varchar(30) not null;
+ALTER TABLE clientes MODIFY apellido varchar(35) not null;
+ALTER TABLE clientes RENAME COLUMN comentarios to Observaciones;
+ALTER TABLE clientes MODIFY Observaciones varchar(255);
+
