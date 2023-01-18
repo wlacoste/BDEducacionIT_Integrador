d5d5f9381e60d501e0939ae7d213f2dccb97782c
walter lacoste <wlacoste@outlook.com>
2023-01-18 15:00:33 -0300

    modulo 3.5

--- Integrador3.sql
+++ Integrador3.sql
@@ -64,6 +64,62 @@ select *, round(Cargo*0.21,2) as IVA, round(Cargo + Cargo*0.21,2) as NETO, floor
 select *, round(Cargo*0.21,2) as IVA, round(Cargo + Cargo*0.21,2) as NETO, floor(Cargo + Cargo*0.21) as 'Redondeo cliente', ceil(Cargo + Cargo*0.21) as 'Redondeo empresa' from pedidos_neptuno;
 
 -- Etapa 3.5
+-- 1
+select sum(1) from pedidos_neptuno;
+select count(*) from pedidos_neptuno;
+select * from pedidos_neptuno;
+
+-- 2
+select count(*) as ENTREGAS_SPEEDY_EXPRESS from pedidos_neptuno where Transportista = 'SPEEDY EXPRESS';
+-- 3
+select count(*) as VENTAS from pedidos_neptuno where Empleado like 'C%';
+-- 4
+select round(avg(PrecioUnidad),2) as PRECIO_PROMEDIO from productos_neptuno;
+-- 5
+select round(avg(PrecioUnidad),2) as PRECIO_PROMEDIO,
+round(min(PrecioUnidad),2) as PRECIO_INFERIOR
+from productos_neptuno;
+-- 6
+select round(avg(PrecioUnidad),2) as PRECIO_PROMEDIO,
+round(min(PrecioUnidad),2) as PRECIO_INFERIOR,
+round(max(PrecioUnidad),2) as PRECIO_MAXIMO
+from productos_neptuno;
+-- 7
+select
+round(max(PrecioUnidad),2) as PRECIO_MAXIMO,
+NombreCategoria as CATEGORIA
+from productos_neptuno
+group by CATEGORIA;
+-- 8
+select count(IdPedido) as ENTREGAS, Transportista from pedidos_neptuno group by Transportista;
+-- 9
+select count(*) as nacimientos, sexo from nacimientos group by sexo; 
+-- 10
+select  nombrecompania as cliente, round(sum(cargo),2) as total_GASTOS from pedidos_neptuno group by cliente;
+-- 11
+select seccion, count(*) as cantidad from productos group by seccion order by cantidad desc;
+-- 12
+select  year(fechapedido) as anio, 
+ month(fechaPedido) as mes,
+ count(*) as ventas 
+ from pedidos_neptuno
+ group by anio, mes
+ order by anio, mes;
+ 
+ -- 13
+ select Empleado ,round(sum(cargo),2) as facturacion from pedidos_neptuno group by Empleado;
+
+ select Empleado ,round(avg(cargo),2) as promedio from pedidos_neptuno group by Empleado;
+
+ select Empleado ,max(cargo) as mejor_venta from pedidos_neptuno group by Empleado;
+
+ select Empleado ,min(cargo) as peor_venta from pedidos_neptuno group by Empleado;
+
+ select Empleado ,count(IdPedido) as maximo from pedidos_neptuno group by Empleado;
+
+
+
+
 
 
 
